66cfaed9e1a8028eecf2b3e15d834442a6e70180
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-09-25 16:41:51 +0200

    Mainjs

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   document.querySelector("form").innerHTML =
     "<h3>Universe managing operation has been a success</h3>";
+    if (e.target[0].selected) {
+      window.close()
+    }
 });
