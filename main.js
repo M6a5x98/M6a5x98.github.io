86d5bf5c8f8a737adb998240f231bf556c39a90d
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-09-26 09:17:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  if (e.target[0].selected) {
   document.querySelector("form").innerHTML =
     "<h3>Universe managing operation has been a success</h3>";
-  } else if (!e.target[0].selected) {
-    alert("Destroying")
-    document.querySelector("body").class="explode"
-  }
 });
