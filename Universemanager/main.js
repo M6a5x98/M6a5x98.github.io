2fe3645ca2ad5f4b13165025e569a6b270cd7ca9
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-09-27 07:33:41 +0200

    Move Files of UniverseManager in a dir of the same name

--- Universemanager/main.js
+++ Universemanager/main.js
@@ -1,5 +1,6 @@
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(e)
   document.querySelector("form").innerHTML =
     "<h3>Universe managing operation has been a success</h3>";
 });
