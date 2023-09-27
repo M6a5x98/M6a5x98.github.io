4e73e6efa7e5812afe127770090784b474dd8347
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-09-27 07:48:00 +0200

    Delete innerHTML line

--- Universemanager/main.js
+++ Universemanager/main.js
@@ -2,7 +2,7 @@ document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(e);
   setTimeout(() => {
-    document.querySelector("form").innerHTML =
-      "<h3>Universe managing operation has been a success</h3>";
+    // document.querySelector("form").innerHTML =
+    //   "<h3>Universe managing operation has been a success</h3>";
   }, 1000);
 });
