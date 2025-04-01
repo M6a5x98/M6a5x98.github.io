f3be922430317c8a192a7453beb932194ed17750
M6a5x98 <m6a5x98@outlook.com>
2025-04-01 14:40:54 +0200

    Started to add a card system for the projects

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ document.querySelectorAll("a").forEach((e) => {
 document.querySelectorAll(".project").forEach((e) => {
   e.style.setProperty("--color", e.getAttribute("color"));
 });
+// Project <card />
+const colors = ["red", "orange", "yellow", "chartreuse", "green", "blue", "aqua"].reverse()
+document.querySelectorAll("card").forEach((e) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <h3>${e.getAttribute("title")}</h3>
+    <ul>
+      <li>Utility : ${e.getAttribute("utility")}/5</li>
+      <li>Complexity : ${e.getAttribute("complexity")}/5</li>
+    </ul>
+  `
+  card.style.setProperty("--utility-color", colors[e.getAttribute("utility")])
+  e.appendChild(card);
+})
